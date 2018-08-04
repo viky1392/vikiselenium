ac0322ce98fb138b6989288e718761eea5e74417
smriti <smriti.sinha86@gmail.com>
2018-08-04 18:20:53 +0530

    new testcase

---
+++ src/main/java/Test1Day1/Arithmeticoperationsgit.java
@@ -0,0 +1,21 @@
+package Test1Day1;
+
+public class Arithmeticoperationsgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int numberone;
+int numberTwo;
+public void addition() {}  //edited by smriti
+public int Substract()
+public int Multiply()
+public void Multiply numberonebyTWo()
+public int divide numberTwoby(10)
+{
+	int numberone= 20;
+	int numberTwo= 20;
+}
+
+	}
+
+}
